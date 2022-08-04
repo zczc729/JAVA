87f79c4296426c8bc71b4abf6ce5034491ba817c
zczc729 <102545749+zczc729@users.noreply.github.com>
2022-08-04 23:13:13 +0900

    day02
    자바 공부 2일차

---
+++ day02/FirstWork.java
@@ -0,0 +1,18 @@
+package day02;
+// 한줄 주석
+/*
+여러줄
+주석 
+ */
+
+// Ctrl + +(-) : 글자 크기 키우기(줄이기)
+
+public class FirstWork {
+// Ctrl + F11 : 실행
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Java!");
+
+	}
+
+}
